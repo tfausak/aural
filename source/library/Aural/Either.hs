aa289a744da8866238aa3b5580686564607806e7
Taylor Fausak <taylor@fausak.me>
2019-06-16 09:29:52 -0400

    Add call stack and documentation to `unsafeFromRight`

--- source/library/Aural/Either.hs
+++ source/library/Aural/Either.hs
@@ -2,7 +2,11 @@ module Aural.Either
   ( unsafeFromRight
   ) where
 
-unsafeFromRight :: Either String a -> a
+import qualified GHC.Stack
+
+-- | Returns the 'Right' value if it's available, otherwise calls 'error' with
+-- the 'Left' value.
+unsafeFromRight :: GHC.Stack.HasCallStack => Either String a -> a
 unsafeFromRight e =
   case e of
     Left l -> error l
