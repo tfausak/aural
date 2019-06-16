ffd5a4795b7c6e2493dd4d2d7b5c1b31915f1b3d
Taylor Fausak <taylor@fausak.me>
2019-06-16 10:33:34 -0400

    Implement UTF-8 encoding

--- source/library/Aural/Utf8.hs
+++ source/library/Aural/Utf8.hs
@@ -25,7 +25,7 @@ valid1 a =
 decode1 :: Data.Word.Word8 -> Char
 decode1 a =
   Data.Char.chr $
-    maskAndShift 0xff 0 a
+    decodeHelper 0x7f 0 a
 
 valid2 :: Data.Word.Word8 -> Data.Word.Word8 -> Bool
 valid2 a b =
@@ -35,8 +35,8 @@ valid2 a b =
 decode2 :: Data.Word.Word8 -> Data.Word.Word8 -> Char
 decode2 a b =
   Data.Char.chr $
-    maskAndShift 0x1f 6 a Data.Bits..|.
-    maskAndShift 0x3f 0 b
+    decodeHelper 0x1f 6 a Data.Bits..|.
+    decodeHelper 0x3f 0 b
 
 valid3 :: Data.Word.Word8 -> Data.Word.Word8 -> Data.Word.Word8 -> Bool
 valid3 a b c =
@@ -47,9 +47,9 @@ valid3 a b c =
 decode3 :: Data.Word.Word8 -> Data.Word.Word8 -> Data.Word.Word8 -> Char
 decode3 a b c =
   Data.Char.chr $
-    maskAndShift 0x0f 12 a Data.Bits..|.
-    maskAndShift 0x3f 6 b Data.Bits..|.
-    maskAndShift 0x3f 0 c
+    decodeHelper 0x0f 12 a Data.Bits..|.
+    decodeHelper 0x3f 6 b Data.Bits..|.
+    decodeHelper 0x3f 0 c
 
 valid4 :: Data.Word.Word8 -> Data.Word.Word8 -> Data.Word.Word8 -> Data.Word.Word8 -> Bool
 valid4 a b c d =
@@ -61,14 +61,63 @@ valid4 a b c d =
 decode4 :: Data.Word.Word8 -> Data.Word.Word8 -> Data.Word.Word8 -> Data.Word.Word8 -> Char
 decode4 a b c d =
   Data.Char.chr $
-    maskAndShift 0x07 18 a Data.Bits..|.
-    maskAndShift 0x3f 12 b Data.Bits..|.
-    maskAndShift 0x3f 6 c Data.Bits..|.
-    maskAndShift 0x3f 0 d
+    decodeHelper 0x07 18 a Data.Bits..|.
+    decodeHelper 0x3f 12 b Data.Bits..|.
+    decodeHelper 0x3f 6 c Data.Bits..|.
+    decodeHelper 0x3f 0 d
 
-maskAndShift :: Data.Word.Word8 -> Int -> Data.Word.Word8 -> Int
-maskAndShift mask shift word8 =
-  Data.Bits.shiftL (fromIntegral (word8 Data.Bits..&. mask)) shift
+decodeHelper :: Data.Word.Word8 -> Int -> Data.Word.Word8 -> Int
+decodeHelper mask shift word8 =
+  let
+    (.&) :: Data.Word.Word8 -> Data.Word.Word8 -> Data.Word.Word8
+    (.&) = (Data.Bits..&.)
 
+    word8ToInt :: Data.Word.Word8 -> Int
+    word8ToInt = fromIntegral
+
+    (.<<) :: Int -> Int -> Int
+    (.<<) = Data.Bits.shiftL
+  in
+    word8ToInt (word8 .& mask) .<< shift
+
+-- | Encodes text as a stream of UTF-8 bytes.
 encode :: String -> [Data.Word.Word8]
-encode _ = []
+encode = concatMap (encodeInt . Data.Char.ord)
+
+encodeInt :: Int -> [Data.Word.Word8]
+encodeInt x
+  | x <= 0x7f =
+    [ encodeHelper 0 0x7f 0x00 x
+    ]
+  | x <= 0x7ff =
+    [ encodeHelper 6 0x3f 0xc0 x
+    , encodeHelper 0 0x3f 0x80 x
+    ]
+  | x <= 0xffff =
+    [ encodeHelper 12 0x0f 0xe0 x
+    , encodeHelper 6 0x3f 0x80 x
+    , encodeHelper 0 0x3f 0x80 x
+    ]
+  | otherwise =
+    [ encodeHelper 18 0x07 0xf0 x
+    , encodeHelper 12 0x3f 0x80 x
+    , encodeHelper 6 0x3f 0x80 x
+    , encodeHelper 0 0x3f 0x80 x
+    ]
+
+encodeHelper :: Int -> Int -> Data.Word.Word8 -> Int -> Data.Word.Word8
+encodeHelper shift mask label int =
+  let
+    (.>>) :: Int -> Int -> Int
+    (.>>) = Data.Bits.shiftR
+
+    (.&) :: Int -> Int -> Int
+    (.&) = (Data.Bits..&.)
+
+    intToWord8 :: Int -> Data.Word.Word8
+    intToWord8 = fromIntegral
+
+    (.|) :: Data.Word.Word8 -> Data.Word.Word8 -> Data.Word.Word8
+    (.|) = (Data.Bits..|.)
+  in
+    label .| intToWord8 ((int .>> shift) .& mask)
--- source/test/Main.hs
+++ source/test/Main.hs
@@ -23,6 +23,7 @@ main = T.hspec . T.describe "Aural" $ do
       T.it "with right" $ do
         Aural.Either.unsafeFromRight (Right ()) `T.shouldBe` ()
 
+  -- https://www.cl.cam.ac.uk/~mgk25/ucs/examples/UTF-8-test.txt
   T.describe "Utf8" $ do
 
     T.describe "decode" $ do
@@ -42,7 +43,6 @@ main = T.hspec . T.describe "Aural" $ do
       T.it "decodes four bytes" $ do
         Aural.Utf8.decode [0xf0, 0x90, 0x8d, 0x88] `T.shouldBe` [Right '\x10348']
 
-      -- https://www.cl.cam.ac.uk/~mgk25/ucs/examples/UTF-8-test.txt
 
       T.it "passes 3.1.9" $ do
         mapM_
@@ -66,7 +66,20 @@ main = T.hspec . T.describe "Aural" $ do
 
     T.describe "encode" $ do
 
-      T.it "is" T.pending
+      T.it "encodes empty input" $ do
+        Aural.Utf8.encode [] `T.shouldBe` []
+
+      T.it "encodes one byte" $ do
+        Aural.Utf8.encode ['\x24'] `T.shouldBe` [0x24]
+
+      T.it "encodes two bytes" $ do
+        Aural.Utf8.encode ['\xa2'] `T.shouldBe` [0xc2, 0xa2]
+
+      T.it "encodes three bytes" $ do
+        Aural.Utf8.encode ['\x20ac'] `T.shouldBe` [0xe2, 0x82, 0xac]
+
+      T.it "encodes four bytes" $ do
+        Aural.Utf8.encode ['\x10348'] `T.shouldBe` [0xf0, 0x90, 0x8d, 0x88]
 
   T.describe "Version" $ do
 
