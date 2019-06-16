6bc66fcf553bf90fa1ad4e0ca04e6695e85f697c
Taylor Fausak <taylor@fausak.me>
2019-06-16 09:00:23 -0400

    Fix test description

--- source/test/Main.hs
+++ source/test/Main.hs
@@ -8,8 +8,9 @@ import qualified Test.Hspec as T
 main :: IO ()
 main = T.hspec . T.describe "Aural" $ do
   T.describe "Either" $ do
-    T.it "with left" $ do
-      let l = "oh no"
-      Aural.Either.unsafeFromRight (Left l) `T.shouldThrow` T.errorCall l
-    T.it "with right" $ do
-      Aural.Either.unsafeFromRight (Right ()) `T.shouldBe` ()
+    T.describe "unsafeFromRight" $ do
+      T.it "with left" $ do
+        let l = "oh no"
+        Aural.Either.unsafeFromRight (Left l) `T.shouldThrow` T.errorCall l
+      T.it "with right" $ do
+        Aural.Either.unsafeFromRight (Right ()) `T.shouldBe` ()
